111bbddd588963e2ac095d73437cdc216bd8741d
deepak748030 <deepakkushwah748930@gmail.com>
2025-08-08 21:57:16 +0530

    added more req per ip

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1000, // limit each IP to 100 requests per windowMs
   message: {
     success: false,
     message: 'Too many requests from this IP, please try again later.'
